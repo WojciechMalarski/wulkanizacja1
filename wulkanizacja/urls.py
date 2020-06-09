2f6229a3624bb388d49d329651cc06ad512b04c6
Jurski <jurski.malarski@espectro.pw>
2020-06-09 17:40:26 +0200

    Dodanie Bootstrap 3
    pip install django-bootstrap4
    
    Testy  {% include "../navbar.html" %}
    
    I logowanie
    
    testy
    
    Dodanie css itp
    
    poprawa kodu
    
    # Dodac zdjecia bo nie działa

--- wulkanizacja/urls.py
+++ wulkanizacja/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('opony/', include('wulkanizacjaweb.urls')),
     path('login/', auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-    path('rejestracja/')
+    path('rejestracja/', auth_views.LoginView.as_view(), name="login")
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
